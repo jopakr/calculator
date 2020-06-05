c3762aea3ccf82431b5f7f8ae84b82ae01ebc39a
Jordan <jpkronquist@gmail.com>
2020-06-04 17:19:18 -0700

    add new line of refracted code.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,10 @@
 function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result = add(number1, number2);
-  alert(result);
+  return number1 + number2;
+}
 
-  function subtract(number1, number2) {
-    return number1 - number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result = subtract(number1, number2);
-  alert(result);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
 
-  function multiply(number1, number2) {
-    return number1 * number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result = multiply(number1, number2);
-  alert(result);
 
-  function divide(number1, number2) {
-    return number1 / number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result = divide(number1, number2);
-  alert(result);
