b77ed743f8a207b92efc9e28f6928baa4e592269
Jordan <jpkronquist@gmail.com>
2020-06-04 18:13:40 -0700

    correct the result variable to result1,result2,result3,result4 to separate each function.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ function add(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+const result1 = add(number1, number2);
+alert(result1);
 
 function subtract(number3, number4) {
   return number3 - number4;
@@ -13,8 +13,8 @@ function subtract(number3, number4) {
 
 const number3 = parseInt(prompt("Enter a number:"));
 const number4 = parseInt(prompt("Enter another number:"));
-const result = subtract(number3, number4);
-alert(result);
+const result2 = subtract(number3, number4);
+alert(result2);
 
 function multiply(number5, number6) {
   return number5 * number6;
@@ -22,8 +22,8 @@ function multiply(number5, number6) {
 
 const number5 = parseInt(prompt("Enter a number:"));
 const number6 = parseInt(prompt("Enter another number:"));
-const result = multiply(number5, number6);
-alert(result);
+const result3 = multiply(number5, number6);
+alert(result3);
 
 function divide(number7, number8) {
   return number7 / number8;
@@ -31,8 +31,8 @@ function divide(number7, number8) {
 
 const number7 = parseInt(prompt("Enter a number:"));
 const number8 = parseInt(prompt("Enter another number:"));
-const result = divide(number7, number8);
-alert(result);
+const result4 = divide(number7, number8);
+alert(result4);
 
 
 
