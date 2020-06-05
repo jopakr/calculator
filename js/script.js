058fc232a11ff1735b0f3fc7b5139bd040d894a6
Jordan <jpkronquist@gmail.com>
2020-06-04 18:05:53 -0700

    add subtract/multiply/divide functions.

--- js/script.js
+++ js/script.js
@@ -7,4 +7,37 @@ const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
 
+function subtract(number3, number4) {
+  return number3 - number4;
+}
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const result = subtract(number3, number4);
+alert(result);
+
+function multiply(number5, number6) {
+  return number5 * number6;
+}
+
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+const result = multiply(number5, number6);
+alert(result);
+
+function divide(number7, number8) {
+  return number7 / number8;
+}
+
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+const result = divide(number7, number8);
+alert(result);
+
+
+
+
+
+
+
 
